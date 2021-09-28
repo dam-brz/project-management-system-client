e7b23f5b2f08cb4548fe8fb6f72d8d4f35c6183a
dam-brz <dam-brz@wp.pl>
2021-09-28 14:47:05 +0200

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Dashboar from './components/Dashboar';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboar}/>
+        <Route exact path="/addProject" component={AddProject}/>
+      </div>
+    </Router>
   );
 }
 
