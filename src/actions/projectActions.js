93eeca75605809de91000d6d672ebd7698d0b067
dam-brz <dam-brz@wp.pl>
2021-09-30 10:24:13 +0200

    Create project reducer and action

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,14 +1,24 @@
 import axios from "axios";
-import { GET_ERRORS } from "./type";
+import { GET_ERRORS, GET_PROJECTS } from "./type";
+
+const PROJECTS_URL = "http://localhost:8080/api/projects";
 
 export const createProject = (project, history) => async dispatch => {
     try {
-        const res = await axios.post("http://localhost:8080/api/projects", project)
+        const res = await axios.post(PROJECTS_URL, project);
         history.push("/dashboard")
     } catch (error) {
         dispatch({
             type:GET_ERRORS,
             peyload:error.response.data
-        })
+        });
     }
-}
+};
+
+export const getProjects = () => async dispatch => {
+    const res = await axios.get(PROJECTS_URL);
+    dispatch({
+        type: GET_PROJECTS,
+        payload: res.data
+    });
+};
