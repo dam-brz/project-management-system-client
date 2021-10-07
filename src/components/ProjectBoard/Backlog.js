5da23d05294afd5f1d253c2be64e213bc66496a4
dam-brz <dam-brz@wp.pl>
2021-10-07 19:31:41 +0200

    Organize project tasks by status and priority

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -5,8 +5,27 @@ class Backlog extends Component {
     render() {
         const {project_tasks_prop} = this.props;
         const tasks = project_tasks_prop.map(project_task => (
-            <ProjectTask key={project_task.id} project_task={project_task}/>
+            <ProjectTask key={project_task.projectSequence} project_task={project_task}/>
         ));
+
+        let toDoTasks = [];
+        let inProgressTasks = [];
+        let doneTasks = [];
+
+        for (let i = 0; i < tasks.length; i++) {
+            if (tasks[i].props.project_task.status === "TO_DO") {
+                toDoTasks.push(tasks[i]);
+            }
+
+            if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+                inProgressTasks.push(tasks[i]);
+            }
+
+            if (tasks[i].props.project_task.status === "DONE") {
+                doneTasks.push(tasks[i]);
+            }
+            
+        }
         
 
         return (
@@ -17,7 +36,7 @@ class Backlog extends Component {
                             <div className="card-header bg-secondary text-white">
                                 <h3>TO DO</h3>
                             </div>
-                            {tasks}
+                            {toDoTasks}
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -25,6 +44,7 @@ class Backlog extends Component {
                             <div className="card-header bg-primary text-white">
                                 <h3>In Progress</h3>
                             </div>
+                            {inProgressTasks}
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -32,6 +52,7 @@ class Backlog extends Component {
                             <div className="card-header bg-success text-white">
                                 <h3>Done</h3>
                             </div>
+                            {doneTasks}
                         </div>
                     </div>
                 </div>
