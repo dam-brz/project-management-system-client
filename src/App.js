a5d299006e76da7cf06b05b8d215ff243c32e867
dam-brz <dam-brz@wp.pl>
2021-10-25 13:55:56 +0200

    Add routes to login and register

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask';
+import LandingPage from './components/Layout/LandingPage';
+import Register from './components/UserManagement/Register';
+import Login from './components/UserManagement/Login';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
       <Router>
         <div className="App">
           <Header />
+          <Route exact path="/" component={LandingPage}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
           <Route exact path="/dashboard" component={Dashboar}/>
           <Route exact path="/addProject" component={AddProject}/>
           <Route exact path="/updateProject/:id" component={UpdateProject}/>
