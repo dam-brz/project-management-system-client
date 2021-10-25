0d6b7c1e6fd8c9852d189d3a9d6daeaf06fa30ce
dam-brz <dam-brz@wp.pl>
2021-10-25 16:18:06 +0200

    Fix error reducer typo

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -5,7 +5,7 @@ const initialState = {};
 export default function updateState(state=initialState, action) {
     switch(action.type) {
         case GET_ERRORS :
-            return action.peyload;
+            return action.payload;
 
         default :
         return state;
