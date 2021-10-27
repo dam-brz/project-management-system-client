ae44da254a648466db8f38acbda74c7f9bb5def4
dam-brz <dam-brz@wp.pl>
2021-10-27 17:57:59 +0200

    Check is token not expired

--- src/App.js
+++ src/App.js
@@ -13,26 +13,50 @@ import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProj
 import LandingPage from './components/Layout/LandingPage';
 import Register from './components/UserManagement/Register';
 import Login from './components/UserManagement/Login';
+import jwt_decode from "jwt-decode";
+import setJWToken from './securityUtils/setJWToken';
+import { SET_CURRENT_USER } from './actions/type';
+import { logout } from './actions/securityActions';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/" component={LandingPage}/>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/dashboard" component={Dashboar}/>
-          <Route exact path="/addProject" component={AddProject}/>
-          <Route exact path="/updateProject/:id" component={UpdateProject}/>
-          <Route exact path="/projectBoard/:projectIdentifier" component={ ProjectBoard }/>
-          <Route exact path="/addProjectTask/:projectIdentifier" component={ AddProjectTask }/>
-          <Route exact path="/updateProjectTask/:projectIdentifier/:projectSequence" component={ UpdateProjectTask }/>
-        </div>
-      </Router>
-    </Provider>
-  );
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    store.dispatch(logout());
+    window.location.href = "/";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+       <Router>
+         <div className="App">
+           <Header />
+           <Route exact path="/" component={LandingPage}/>
+           <Route exact path="/register" component={Register}/>
+           <Route exact path="/login" component={Login}/>
+           <Route exact path="/dashboard" component={Dashboar}/>
+           <Route exact path="/addProject" component={AddProject}/>
+           <Route exact path="/updateProject/:id" component={UpdateProject}/>
+           <Route exact path="/projectBoard/:projectIdentifier" component={ ProjectBoard }/>
+           <Route exact path="/addProjectTask/:projectIdentifier" component={ AddProjectTask }/>
+           <Route exact path="/updateProjectTask/:projectIdentifier/:projectSequence" component={ UpdateProjectTask }/>
+         </div>
+       </Router>
+     </Provider>
+    );
+  }
 }
 
 export default App;
