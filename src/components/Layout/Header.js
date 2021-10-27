99b728f6093e390d222b095d9d864775984efb90
dam-brz <dam-brz@wp.pl>
2021-10-27 19:53:50 +0200

    Create header based on security

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { logout } from '../../actions/securityActions';
 
-export default class Header extends Component {
+class Header extends Component {
+
+    logout() {
+        this.props.logout();
+        window.location.href = "/";
+      }
+
     render() {
+        const {validToken, user}  = this.props.security;
+
+        const userIsAuth = (
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="mobile-nav">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to="/dashboard" className="nav-link">
+                            Log in as: {user.username}
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link to="/logout" className="nav-link" onClick={this.logout.bind(this)}>
+                            Log out
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        const userIsNotAuth = (
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="mobile-nav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link to="/register" className="nav-link">
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/login" className="nav-link">
+                            Log In
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        let headerLinks;
+
+        if (validToken && user) {
+            headerLinks = userIsAuth;
+        } else {
+            headerLinks = userIsNotAuth;
+        }
+
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
                 <div className="container">
@@ -13,30 +68,21 @@ export default class Header extends Component {
                         <span className="navbar-toggler-icon" />
                     </button>
 
-                    <div className="collapse navbar-collapse" id="mobile-nav">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link to="/dashboard" className="nav-link">
-                                    Dashboard
-                                </Link>
-                            </li>
-                        </ul>
-
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link to="/register" className="nav-link ">
-                                    Sign Up
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link">
-                                    Login
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                    {headerLinks}
+                    
                 </div>
             </nav>
         )
     }
 }
+
+Header.propTypes = {
+    logout: PropTypes.func.isRequired,
+    security: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    security: state.security
+})
+
+export default connect(mapStateToProps, {logout}) (Header);
